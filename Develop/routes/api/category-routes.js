fe7934a523f42becac4025651c21b94f7c1217ad
Banksandrew16 <88170202+Banksandrew16@users.noreply.github.com>
2021-11-07 16:11:14 -0500

    first commit

---
+++ Develop/routes/api/category-routes.js
@@ -0,0 +1,72 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  Category.findAll({
+    include: [
+      Product
+    ]
+  })
+  .then(dbData => res.json(dbData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  Category.findOne({
+    include: [
+      Product
+    ],
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbData => res.json(dbData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.post('/', (req, res) => {
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbData => res.json(dbData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.put('/:id', (req, res) => {
+  Category.update({category_name: req.body.category_name}, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbData => res.json(dbData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbData => res.json(dbData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+module.exports = router;
---
+++ README.md
@@ -0,0 +1,14 @@
+# E-commerce Back End 
+
+
+## Description
+```
+This is the back end for an ecommerce app
+```
+
+---
+
+## Usage
+```
+Navigate to the root directory in the command line. Then enter node server.js
+```
